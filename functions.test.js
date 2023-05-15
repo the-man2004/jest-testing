0b64b20c5414e5a9d4c803992945f707d5d44953
the-man2004 <heinrichhb111@gmail.com>
2023-05-15 15:06:46 -0700

    Finnished tutorial

--- functions.test.js
+++ functions.test.js
@@ -1,5 +1,29 @@
 functions = require("./functions");
 
+// beforeEach(() => initDatabase());
+// afterEach(() => closeDatabase());
+
+// beforeAll(() => initDatabase());
+// afterAll(() => closeDatabase());
+
+// const initDatabase = () => console.log("Database initialized");
+// const closeDatabase = () => console.log("Database closed...");
+const nameCheck = () => console.log("Checking Name.....");
+
+describe("Checking Names", () => {
+  beforeEach(() => nameCheck());
+
+  test("User is Jeff", () => {
+    const user = "Jeff";
+    expect(user).toBe("Jeff");
+  });
+
+  test("User is Karen", () => {
+    const user = "Karen";
+    expect(user).toBe("Karen");
+  });
+});
+
 // toBe
 test("Adds 2 + 2 to equal 4", () => {
   expect(functions.add(2, 2)).toBe(4);
