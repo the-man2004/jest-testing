97a9c3f076b2b327f91ce26cabee2c6a1f6bfe61
the-man2004 <heinrichhb111@gmail.com>
2023-05-15 12:43:05 -0700

    Watched tutorial 16:30 minutes

---
+++ functions.test.js
@@ -0,0 +1,36 @@
+functions = require("./functions");
+
+// toBe
+test("Adds 2 + 2 to equal 4", () => {
+  expect(functions.add(2, 2)).toBe(4);
+});
+
+// not
+test("Adds 2 + 2 to NOT equal 5", () => {
+  expect(functions.add(2, 2)).not.toBe(5);
+});
+
+// CHECK FOR TRUTHY & FALSY VALUES
+// toBeNull matches only null
+// toBeUndefined matches only undefined
+// toBeDefined is the opposite of toBeUndefined
+// toBeTruthy matches anything that an if statement treats as true
+// toBeFalsy matches anything that an if statement treats as false
+
+// toBeNull
+test("Should be null", () => {
+  expect(functions.isNull()).toBe(null);
+});
+
+// toBeFalsy
+test("Should be falsy", () => {
+  expect(functions.checkValue(null)).toBeFalsy();
+});
+
+// toEqual
+test("user should be Brad Traversy obj", () => {
+  expect(functions.createUser()).toEqual({
+    firstName: "Brad",
+    lastName: "Traversy",
+  });
+});
